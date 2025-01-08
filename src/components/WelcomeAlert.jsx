5db9f02aea621edee40282d92561902232458ba8
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-01-08 17:34:27 +0100

    comm

--- src/components/WelcomeAlert.jsx
+++ src/components/WelcomeAlert.jsx
@@ -1,13 +1,17 @@
-import { Alert, } from "react-bootstrap"
+import { Alert } from "react-bootstrap"
 
 function WelcomeAlert() {
-     return (
-      <>
-        <Alert variant="success" className="mb-0 pb-3">
-          <Alert.Heading >BENVENUTO su Bookshop.it!</Alert.Heading>
-        </Alert>       
-      </>
-    );
-  }
-  
-  export default WelcomeAlert;
+  return (
+    <>
+      <Alert variant="info" className="mb-0 pb-3">
+        <Alert.Heading>BENVENUTO su Bookshop.it!</Alert.Heading>
+        <hr />
+        <p className="mb-0">
+          La tua libreria preferita dove troverai sempre nuove uscite e libri provenienti da tutto il mondo!
+        </p>
+      </Alert>
+    </>
+  )
+}
+
+export default WelcomeAlert
